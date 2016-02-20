556004cd7f4f4504c0c9a998a4501eb59de8946c
Raphael Gontijo Lopes <raphaelgontijolopes@gmail.com>
2016-02-20 00:26:18 -0500

    initial flask setup. hello world server

---
+++ app/views.py
@@ -0,0 +1,12 @@
+from app import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return 'Hello World'
+
+
+@app.route('/user/<username>')
+def stats(username):
+    # Call functions that compute stats for user
+    return username + ' statistics: you sleep too much. #sleep is for the weak.'
