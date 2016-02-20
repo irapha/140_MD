157d28395374ab561e44dc1e521d54bc0370c1e7
Raphael Gontijo Lopes <raphaelgontijolopes@gmail.com>
2016-02-20 15:03:45 -0500

    take utc offset into account, return data in d3-compatible format

--- app/stats.py
+++ app/stats.py
@@ -1,10 +1,15 @@
 from datetime import datetime
 
 def count(tweets):
+    if not tweets:
+        return {}
+
+    utc_offset = tweets[0]['user']['utc_offset'] / 3600
+
     counts = dict()
     for tweet in tweets:
         timestamp = datetime.strptime(tweet['created_at'], '%a %b %d %H:%M:%S +0000 %Y')
-        minutes = (timestamp.hour * 60 + timestamp.minute)
+        minutes = (((timestamp.hour + utc_offset) % 24) * 60 + timestamp.minute)
         if minutes not in counts:
             counts[minutes] = 0
         counts[minutes] += 1
@@ -20,14 +25,20 @@ def get_poisson_dists(tweets):
             if timestep - 60 <= t < timestep + 60 or \
                timestep - 60 <= t - 24*60 < timestep + 60 or \
                timestep - 60 <= t + 24*60 < timestep + 60:
-                probs[timestep] += counts[t]
+                probs[timestep] += counts[t] / 60
     return probs
 
 def get_statistics(tweets):
-    probs = get_poisson_dists(tweets)
+    probs = jsonify_dist(get_poisson_dists(tweets))
     stats = {
         'well_rested': True,
         'rest_percentage': 17,
         'probs': probs
         }
     return stats
+
+def jsonify_dist(probs):
+    json = []
+    for k in probs:
+        json.append([k, probs[k]])
+    return json
